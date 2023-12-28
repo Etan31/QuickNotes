ab19d843ec1df2b741f5c80efded4d78f9900cd2
Etan31 <tumbagatristanehron31@gmail.com>
2023-12-29 00:00:40 +0800

    Save query for password manipulation

--- app/src/main/java/com/example/quicknotes/PasswordDao.java
+++ app/src/main/java/com/example/quicknotes/PasswordDao.java
@@ -1,6 +1,7 @@
 package com.example.quicknotes;
 
 import androidx.room.Dao;
+import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
@@ -13,4 +14,13 @@ public interface PasswordDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     void savePassword(PasswordEntity password);
+
+//    @Delete
+//    void deletePassword(PasswordEntity passwordEntity);
+//    @Query("DELETE FROM passwords WHERE id = 1")
+//    void deleteSavedPassword();
+
+    @Query("SELECT * FROM passwords WHERE id = 1 LIMIT 1")
+    PasswordEntity getSavedPassword();
+
 }
