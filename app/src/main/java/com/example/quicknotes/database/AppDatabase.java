c4300477a9d1e001b2bc4bd7a119317c8ba9842b
Etan31 <tumbagatristanehron31@gmail.com>
2024-01-05 21:35:06 +0800

    can now add pdf file to the database, but still not shown.

--- app/src/main/java/com/example/quicknotes/database/AppDatabase.java
+++ app/src/main/java/com/example/quicknotes/database/AppDatabase.java
@@ -6,13 +6,16 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
+import com.example.quicknotes.dao.FileDao;
+import com.example.quicknotes.entities.FileEntity;
 import com.example.quicknotes.password.PasswordDao;
 import com.example.quicknotes.password.PasswordEntity;
 
-@Database(entities = {PasswordEntity.class}, version = 1, exportSchema = false)
+@Database(entities = {PasswordEntity.class, FileEntity.class}, version = 2, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
 
     public abstract PasswordDao passwordDao();
+    public abstract FileDao fileDao();  // Add this line
 
     private static volatile AppDatabase INSTANCE;
 
@@ -22,7 +25,8 @@ public abstract class AppDatabase extends RoomDatabase {
                 if (INSTANCE == null) {
                     INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
                                     AppDatabase.class, "app_database")
-                            .build();
+                                    .fallbackToDestructiveMigration()
+                                    .build();
                 }
             }
         }
