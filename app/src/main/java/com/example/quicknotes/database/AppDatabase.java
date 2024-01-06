f0cb27daf4f82e9a14e3bd6175e29cee180f193b
Etan31 <tumbagatristanehron31@gmail.com>
2024-01-06 12:15:32 +0800

    Revert "can now add pdf file to the database, but still not shown."
    This reverts commit c4300477a9d1e001b2bc4bd7a119317c8ba9842b.

--- app/src/main/java/com/example/quicknotes/database/AppDatabase.java
+++ app/src/main/java/com/example/quicknotes/database/AppDatabase.java
@@ -6,16 +6,13 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
-import com.example.quicknotes.dao.FileDao;
-import com.example.quicknotes.entities.FileEntity;
 import com.example.quicknotes.password.PasswordDao;
 import com.example.quicknotes.password.PasswordEntity;
 
-@Database(entities = {PasswordEntity.class, FileEntity.class}, version = 2, exportSchema = false)
+@Database(entities = {PasswordEntity.class}, version = 1, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
 
     public abstract PasswordDao passwordDao();
-    public abstract FileDao fileDao();  // Add this line
 
     private static volatile AppDatabase INSTANCE;
 
@@ -25,8 +22,7 @@ public abstract class AppDatabase extends RoomDatabase {
                 if (INSTANCE == null) {
                     INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
                                     AppDatabase.class, "app_database")
-                                    .fallbackToDestructiveMigration()
-                                    .build();
+                            .build();
                 }
             }
         }
